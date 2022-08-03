69d0319a132a6d61725a2e805123e81b56dc86a8
AI SCIENCES <49445127+AISPUBLISHING@users.noreply.github.com>
2022-08-03 13:04:19 +0500

    Add files via upload

---
+++ How to Find Unused or Dead Part of a Python Program Automatically.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+get_ipython().system('pip install vulture')
+
+
+# In[4]:
+
+
+a, b, c = 20, 10, 5
+
+def fun1():
+    summ = a+b
+    print(sum)
+    
+def fun2():
+    b = 100
+    print(b)
+    
+    
+fun2()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
